7bbf390fd5453558798f16aaef147d5bcaad5e8d
jeffery-ot <jeffery.owusu@amalitechtraining.org>
2025-04-17 08:21:57 +0000

    database design and schema implementation

--- sql_scripts/phase_1.sql
+++ sql_scripts/phase_1.sql
@@ -1,12 +1,43 @@
 -- Phase 1: Database Design and Schema Implementation
--- Create Products table
+/*CREATE TABLE (); was used as a data definition language(DDL) 
+which helped in creating the tables 
+
+CONSTRAINTS were used to enforce data integrity |
+
+SERIAL is a constraint that auto-increments-
+it helped in indexing the tables as a constraint for the respective columns
+
+NOT NUll ensures the column has no empty/missing values
+
+PRIMARY KEY helps dedicate a column as the primary key of the table
+
+INT ensures the values for the column are integers
+
+DECIMAL ensures the values for the column are decimal numbers with
+DECIMAL(10, 2) meaning 10 digits in total with 2 after the decimal point
+
+CHECK ensures the value being added conforms to the conditions stated
+
+DEFAULT ensures the value stated is used whenever there's no value being inserted
+
+UNIQUE ensures no two values in a column are the same 
+
+TIMESTAMP sets the data type of the column to date and time
+
+VARCHAR ensures the values of the column are variable and can be set to a specific length
+ 
+REFERENCES helps assign a column as a foreign key for another column
+-this creates relationships between different tables
+
+*/
+
 CREATE  TABLE products (
-    product_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    category VARCHAR(50) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL CHECK (price > 0),
-    stock_quantity INT NOT NULL DEFAULT 0 CHECK (stock_quantity >= 0),
-    reorder_level INT NOT NULL DEFAULT 10 CHECK (reorder_level > 0)
+    product_id SERIAL PRIMARY KEY 
+,   name VARCHAR(100) NOT NULL
+,   category VARCHAR(50) NOT NULL
+,   price DECIMAL(10, 2) NOT NULL CHECK (price > 0)
+,   stock_quantity INT NOT NULL DEFAULT 0 CHECK (stock_quantity >= 0)
+,   reorder_level INT NOT NULL DEFAULT 10 CHECK (reorder_level > 0)
 );
 
 -- Customers table
@@ -32,7 +63,7 @@ CREATE TABLE order_details (
 ,   product_id INT NOT NULL REFERENCES products(product_id)
 ,   quantity INT NOT NULL CHECK (quantity > 0)
 ,   unit_price DECIMAL(10, 2) NOT NULL CHECK (unit_price >= 0)
-,    subtotal DECIMAL(12, 2) NOT NULL
+,   subtotal DECIMAL(12, 2) NOT NULL
 );
 
 -- Inventory Logs table
@@ -41,9 +72,19 @@ CREATE TABLE inventory_logs (
 ,   product_id INT NOT NULL REFERENCES products(product_id)
 ,   change_quantity INT NOT NULL
 ,   change_reason VARCHAR(50) NOT NULL
-,   reference_id INT	-- order_id or replenishment_id
+,   reference_id INT	-- this represents order_id or replenishment_id
 ,   previous_quantity INT NOT NULL
 ,   new_quantity INT NOT NULL
 ,   change_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+/*
+CREATE INDEX (); was used to create an index on the specified column
+-an index is a data structure that improves the speed of data retrieval operations on a database table
+*/
+
+-- Creating indexes on commonly queried fields/columns to speed up searches
+CREATE INDEX idx_orders_customer_id ON orders(customer_id);
+CREATE INDEX idx_order_details_order_id ON order_details(order_id);
+CREATE INDEX idx_order_details_product_id ON order_details(product_id);
+CREATE INDEX idx_inventory_logs_product_id ON inventory_logs(product_id);
